387fa15bfbd670a1a66966409761e6dddaeb3d1d
左星辰 <zuoxingchen@fcbox.com>
2020-05-09 13:57:19 +0800

    编译FFmpeg for android导入项目并初次调用成功

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <string>
+extern "C"{
+    #include <libavcodec/avcodec.h>
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_zxc_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    hello += avcodec_configuration();
+    return env->NewStringUTF(hello.c_str());
+}
